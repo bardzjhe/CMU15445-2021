e7c38ca06829e10b1233471979b0e0248b018718
Anthony HO <anthony.ct.ho@outlook.com>
2022-07-30 19:34:37 +0800

    Update p0_starter.h

--- src/include/primer/p0_starter.h
+++ src/include/primer/p0_starter.h
@@ -35,7 +35,13 @@ class Matrix {
    * @param cols The number of columns
    *
    */
-  Matrix(int rows, int cols) {}
+  Matrix(int rows, int cols) {
+    if(rows <= 0 || cols <= 0){
+      rows_ = cols_ = -1;
+    }else{
+      linear_ = new T[rows * cols];
+    }
+  }
 
   /** The number of rows in the matrix */
   int rows_;
@@ -95,7 +101,7 @@ class Matrix {
    * Destroy a matrix instance.
    * TODO(P0): Add implementation
    */
-  virtual ~Matrix() = default;
+  virtual ~Matrix(){delete[] linear_;}
 };
 
 /**
@@ -112,19 +118,28 @@ class RowMatrix : public Matrix<T> {
    * @param rows The number of rows
    * @param cols The number of columns
    */
-  RowMatrix(int rows, int cols) : Matrix<T>(rows, cols) {}
+  RowMatrix(int rows, int cols) : Matrix<T>(rows, cols) {
+    if(rows <= 0 || cols <= 0){
+
+    }else{
+      data_ = new T *[this->rows_ * this->cols_];
+      for(int row=0; row < this->rows_; row++){
+        data_[row] = &this->linear_[row * this->cols_];
+      }
+    }
+  }
 
   /**
    * TODO(P0): Add implementation
    * @return The number of rows in the matrix
    */
-  auto GetRowCount() const -> int override { return 0; }
+  auto GetRowCount() const -> int override { return this->rows_; }
 
   /**
    * TODO(P0): Add implementation
    * @return The number of columns in the matrix
    */
-  auto GetColumnCount() const -> int override { return 0; }
+  auto GetColumnCount() const -> int override { return this->cols_; }
 
   /**
    * TODO(P0): Add implementation
@@ -139,7 +154,11 @@ class RowMatrix : public Matrix<T> {
    * @throws OUT_OF_RANGE if either index is out of range
    */
   auto GetElement(int i, int j) const -> T override {
-    throw NotImplementedException{"RowMatrix::GetElement() not implemented."};
+    if(i>= this->rows_ || j>= this->cols_ || i<0 || j<0){
+      throw NotImplementedException{"RowMatrix::GetElement() not implemented."};
+    }
+    return data_[i][j];
+    
   }
 
   /**
@@ -152,7 +171,12 @@ class RowMatrix : public Matrix<T> {
    * @param val The value to insert
    * @throws OUT_OF_RANGE if either index is out of range
    */
-  void SetElement(int i, int j, T val) override {}
+  void SetElement(int i, int j, T val) override {
+    if(i>= this->rows_ || j>= this->cols_ || i<0 || j<0){
+      throw NotImplementedException{"RowMatrix::GetElement() not implemented."};
+    }
+    data_[i][j] = val;
+  }
 
   /**
    * TODO(P0): Add implementation
@@ -166,7 +190,16 @@ class RowMatrix : public Matrix<T> {
    * @throws OUT_OF_RANGE if `source` is incorrect size
    */
   void FillFrom(const std::vector<T> &source) override {
-    throw NotImplementedException{"RowMatrix::FillFrom() not implemented."};
+    if(static_cast<int> (source.size()) != this->cols_ * this->rows_){
+      throw NotImplementedException{"RowMatrix::FillFrom() not implemented."};
+    }
+
+    for(int r; r<this->rows_; r++){
+      for(int c; c<this->cols_; c++){
+        SetElement(r, c, source[this->cols_*r + c]);
+      }
+    }
+    
   }
 
   /**
@@ -174,7 +207,7 @@ class RowMatrix : public Matrix<T> {
    *
    * Destroy a RowMatrix instance.
    */
-  ~RowMatrix() override = default;
+  ~RowMatrix() override {delete[] data_; }
 
  private:
   /**
